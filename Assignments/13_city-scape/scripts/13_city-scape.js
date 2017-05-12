1ba5a8edf7e76f606b5c594704e52ebe908c6c8b
ccleveland1 <chrstnclvlnd1996@gmail.com>
2017-05-11 18:07:45 -0700

    Still working on it.

--- Assignments/13_city-scape/scripts/13_city-scape.js
+++ Assignments/13_city-scape/scripts/13_city-scape.js
@@ -1,26 +1,38 @@
-////psuedo code
-//user enters city name
-//listen for the submit
-//when user clicks submit
-/// change body background accordingly
-//when user enters city name
-  //listen for the submit then
-    //clear background and change body background accordingly
+    //psuedo code//
+// when form is sumitted
+//red entered city
+//chekc if supported
+//update background to supported city
+//else
+//alert "city is not supporterd"
 
-function toggleGray() {
-  $('body').css('background-color', 'gray');
-  $('body').css('color', 'white');
+
+//where do I implement the $.val() to reset the attribute of the first match?
+var city
+var cityName
+
+
+else if(city === 'SF' || city ==='San Francisco' || city ==='San Fran'){
+  $('body').addClass('SF');
 }
-function toggleWhite() {
-  $('body').css('background-color', 'white');
-  $('body').css('color', 'black');
+else if(city === 'NYC' || city ==='New York City ' || city ==='New York'){
+  $('body').addClass('NYC');
+}
+else if(city === 'Austin'){
+  $('body').addClass('AUSTIN');
+}  
+else if(city === 'LA' || city=== 'la' || city === 'Los Angeles') {
+  $('body').addClass('LA');
+  }
+else if(city === 'SYD' || city ==='Sydney'){
+  $('body').addClass('SYDNEY');
 }
 
-$().click(toggleSF);
-$().click(toggleNYC);
-$().click(toggleAUSTIN);
-$().click(toggleLA);
-$().click(toggleSYDNEY);
+else{
+  alert('city is not supported');
+}
+
+
 
 
 
