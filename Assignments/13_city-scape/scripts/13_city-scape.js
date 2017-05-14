62cc5452259c45a35e841028eeb5bb03a710b324
ccleveland1 <chrstnclvlnd1996@gmail.com>
2017-05-13 22:21:49 -0700

    First attempt at making all of the conditionals. Not complete

--- Assignments/13_city-scape/scripts/13_city-scape.js
+++ Assignments/13_city-scape/scripts/13_city-scape.js
@@ -6,33 +6,80 @@
 //else
 //alert "city is not supporterd"
 
+//hey jquery go find me the document and when it is ready
+//go do something
+$(document).ready(function(){
+  //execute the whole code
+  console.log('JS is loading');
+  //where do I implement the $.val() to reset the attribute of the first match?
+  var city;
 
-//where do I implement the $.val() to reset the attribute of the first match?
-var city
-var cityName
+  $('#city-picker').submit(function() {
+    console.log('form was submitted');
+    console.log(event);
+    //tell the browser not to run the default behaviour
+    //for the form submit
+  event.preventDefault();
 
+    // read the contents of input#city-type
+    // save those contents to var city
+  var cityName = $('#city-type').val();
+      //change to lowercase to simplify logi and address typing inconsistencies 
+  cityName = cityName.toLowerCase();
+  console.log(cityName);
 
-else if(city === 'SF' || city ==='San Francisco' || city ==='San Fran'){
-  $('body').addClass('SF');
-}
-else if(city === 'NYC' || city ==='New York City ' || city ==='New York'){
-  $('body').addClass('NYC');
-}
-else if(city === 'Austin'){
-  $('body').addClass('AUSTIN');
-}  
-else if(city === 'LA' || city=== 'la' || city === 'Los Angeles') {
-  $('body').addClass('LA');
+  var existingBodyClasses = $('body').attr('class');
+  $('body').removeClass('existingBodyClasses');
+
+  switch (cityName) {
+    case 'sf':
+     $('body').addClass('sf');
+      break;
+    case 'nyc':
+     $('body').addClass('nyc');
+      break;
+    case 'la':
+     $('body').addClass('la');
+      break;
+    case 'austin':
+     $('body').addClass('austin');
+      break;
+    case undefined:
+      alert('City is not defined');
+    default:
+      alert('City is not supported');
+
   }
-else if(city === 'SYD' || city ==='Sydney'){
-  $('body').addClass('SYDNEY');
-}
+});
+});
 
-else{
-  alert('city is not supported');
-}
+//    if(cityName === 'sf' || cityName ==='san francisco' || cityName ==='san fran'){
+  //    $('body').addClass('sf');
+   // }
+   // else if(cityName === 'nyc' || cityName ==='new york city' || cityName ==='new york'){
+    // $('body').addClass('nyc');
+   // }
+  //  else if(cityName === 'austin'){
+  //    $('body').addClass('austin');
+   // }  
+   // else if(cityName === 'la' || cityName === 'los angeles') {
+  //   $('body').addClass('la');
+  //  }
+   // else if(cityName === 'syd' || cityName ==='sydney'){
+   //  $('body').addClass('sydney');
+   // }
+
+   // else{
+    //  alert('city is not supported');
+   // }
+
+// });
 
 
 
 
 
+
+// $('input[type-text').keyup(function() {
+ //   city = $(this).val();
+//
---
+++ Assignments/14_rock_paper_scissors/scripts/rock_paper_scissors.js
@@ -0,0 +1,109 @@
+console.log('hello world');
+
+$(document).ready(function(){
+
+  var botPlay = 'Rock'
+  var botPlay = 'Scissors'
+  var botPlay = 'Paper'
+
+
+//add my logic in as if bots respone will be rock. 
+//dont worry about randomizaton yet
+
+
+})
+//listen for event on game options
+//if 
+  //rock is clicked
+  //run randomization
+  //print one of 3 random options
+  //if 
+    //rock vs scissors
+    //rock wins
+  //else if
+    //rock vs paper
+    //rock loses
+  //else if
+    //rock vs rock
+    //tie game
+    //change score
+    if(userPlay === 'Rock')
+      if(botPlay = 'Rock') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Tied')
+    } 
+    else if(botPlay = 'Paper') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Lose')
+    }
+    else if(botPlay = 'Scissors') {
+      document.getElementbyId('').innerHTML
+      console.log('You Win')
+    }
+
+    if(userPlay === 'Paper')
+
+       if(botPlay = 'Rock') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Win')
+    } 
+    else if(botPlay = 'Paper') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Tied')
+    }
+    else if(botPlay = 'Scissors') {
+      document.getElementbyId('').innerHTML
+      console.log('I Win')
+    }
+
+    if(userPlay === 'scissors')
+       if(botPlay = 'Rock') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Lose')
+    } 
+    else if(botPlay = 'Paper') {
+      document.getElementbyId('').innerHTML  
+      console.log('You Win')
+    }
+    else if(botPlay = 'Scissors') {
+      document.getElementbyId('').innerHTML
+      console.log('You Tied')
+    }
+  
+
+
+
+
+
+
+//else if 
+  //paper is clicked
+  //run randomization
+  //print one of three options
+  //if 
+    //paper vs rock
+    //paper wins
+  //else if 
+    //paper vs scissors
+    //paper loses
+  //else if
+    //paper vs paper
+    //tie game
+    //change score
+
+//else if
+  //scissors is clicked
+  //run randomization
+  //print one of three options
+    //if 
+      //scissors vs paper
+      //scissors wins
+    //else if
+      //scissors vs rock
+      //scissors loses
+    //else if
+      //scissors vs scissors
+      //tie game
+      //change score
+
+  //print one of three options
